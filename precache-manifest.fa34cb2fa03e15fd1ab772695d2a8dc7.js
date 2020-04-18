8ab0ddcc361a80ba30bcf90c6f4cd520151956b8
stupidMoewe <martintefra@gmail.com>
2020-04-18 09:23:18 +0200

    Updates

---
+++ precache-manifest.fa34cb2fa03e15fd1ab772695d2a8dc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98d0ff9e60d3127a597bc456e4424db",
+    "url": "//stupidmoewe.github.io/louvetauxlacote/index.html"
+  },
+  {
+    "revision": "edd1923aca27b1acdf2a",
+    "url": "//stupidmoewe.github.io/louvetauxlacote/static/css/main.bdbded33.chunk.css"
+  },
+  {
+    "revision": "5b067d5a8b7b0c3c62f6",
+    "url": "//stupidmoewe.github.io/louvetauxlacote/static/js/2.dd86159b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//stupidmoewe.github.io/louvetauxlacote/static/js/2.dd86159b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd1923aca27b1acdf2a",
+    "url": "//stupidmoewe.github.io/louvetauxlacote/static/js/main.b8ed6ff1.chunk.js"
+  },
+  {
+    "revision": "895c87698a3015d4c48b",
+    "url": "//stupidmoewe.github.io/louvetauxlacote/static/js/runtime-main.f28739f6.js"
+  },
+  {
+    "revision": "c75259c29c9630a4efca0dffed7d3542",
+    "url": "//stupidmoewe.github.io/louvetauxlacote/static/media/logofse.c75259c2.png"
+  }
+]);
